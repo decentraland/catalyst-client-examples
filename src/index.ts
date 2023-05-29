0b6c7ace0a3711cab3c8fec8623c87c1b6a137df
Hugo Arregui <hugo@decentraland.org>
2023-05-29 16:40:33 -0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-/**
- * A function that does something
- * @public
- */
-export function example(){
-  return true
+import { createCatalystClient } from "dcl-catalyst-client"
+import { createFetchComponent } from "@well-known-components/fetch-component"
+
+const CATALYST_URL = "https://peer.decentraland.org"
+const address = "0x5e5D9D1dfD87E9B8B069B8e5d708dB92bE5ADe99"
+
+export async function example() {
+  const fetcher = createFetchComponent()
+  const client = await createCatalystClient({ url: CATALYST_URL, fetcher })
+  const lambdasClient = await client.getLambdasClient()
+  const wearables = await lambdasClient.getWearables(address)
+  const names = await lambdasClient.getNames(address)
+
+  console.log("Wearables: ", wearables.elements)
+  console.log("Names: ", names.elements)
 }
+
+example().catch(console.error)
