5de96b6ba60680cb8029428739d96b29c99f53ad
Hugo Arregui <hugo@decentraland.org>
2023-07-07 12:49:53 -0300

    fetch profiles example

---
+++ src/fetch-profiles.ts
@@ -0,0 +1,16 @@
+import { createCatalystClient } from 'dcl-catalyst-client'
+import { createFetchComponent } from '@well-known-components/fetch-component'
+
+const CATALYST_URL = 'https://peer.decentraland.org'
+const address = '0x5e5D9D1dfD87E9B8B069B8e5d708dB92bE5ADe99'
+
+export async function main() {
+  const fetcher = createFetchComponent()
+  const client = createCatalystClient({ url: CATALYST_URL, fetcher })
+  const lambdasClient = await client.getLambdasClient()
+  const profiles = await lambdasClient.getAvatarsDetailsByPost({ ids: [address] })
+
+  console.log('Profiles: ', profiles)
+}
+
+main().catch(console.error)
